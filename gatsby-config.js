380ae5b318ae7a05915376e28fe1d5fef4e77cee
yuresilva <nogueiranetoy@gmail.com>
2020-01-31 15:56:35 -0300

    feat: add sharp configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,65 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Rocketseat`,
+    siteUrl: `https://rocketseat.com.br/`,
+    description: `As melhores tecnologias em programação. direto ao ponto certo`,
+    social: [
+      {
+        name: "github",
+        url: "https://github.com/yuresilva",
+      },
+      {
+        name: "twitter",
+        url: "https://twitter.com/yuresilva",
+      },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rockeseat`,
+        short_name: `Rockeseat`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#7159c1`,
+        display: `standalone`,
+        icon: `src/assets/icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#7159c1`,
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== "production",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: [`**/styles.js`],
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 }
---
+++ src/pages/about/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Container } from "./styles"
+export default function About() {
+  return (
+    <Container>
+      <h1> Hello meus amigos</h1>
+    </Container>
+  )
+}
